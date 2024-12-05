8b5ea243e732a8fcac6354706e9bb664bbed6644
Alexey <skulkyr20@gmail.com>
2024-12-05 11:49:50 +0300

    Initial commit

---
+++ src/test/java/org/pogonin/shortlinkservice/ShortLinkServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pogonin.shortlinkservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortLinkServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
